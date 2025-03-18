1c669b6a65af5b1781936e252a3ba50099fdbab1
Takeshida-14 <axel.sansor@gmail.com>
2025-03-18 16:12:47 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function validarEntradaNombre(event) {
+    const key = event.key;
+    if (!/^[a-zA-Z\s]+$/.test(key)) {
+        event.preventDefault();
+    }
+}
+function validarEntradaApellidos(event) {
+    const key = event.key;
+    if (!/^[a-zA-Z\s]+$/.test(key)) {
+        event.preventDefault();
+    }
+}
+
+function validarEntradaEdad(event) {
+    const key = event.key;
+    if (!/^[0-9]+$/.test(key)) {
+        event.preventDefault();
+    }
+}
